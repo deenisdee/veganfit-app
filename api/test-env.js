ede7ab27f5c518049e91759902c76a32426f5dd9
Denis Carvalho <47404461+deenisdee@users.noreply.github.com>
2026-01-18 13:18:27 -0300

    Update test-env.js

--- api/test-env.js
+++ api/test-env.js
@@ -2,6 +2,9 @@ module.exports = async (req, res) => {
   res.status(200).json({
     allEnvKeys: Object.keys(process.env),
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-    MP_ACCESS_TOKEN: process.env.MP_ACCESS_TOKEN?.substring(0, 20) + '...'
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+    FIREBASE_PRIVATE_KEY_EXISTS: !!process.env.FIREBASE_PRIVATE_KEY,
+    MP_ACCESS_TOKEN_EXISTS: !!process.env.MP_ACCESS_TOKEN,
+    MP_PUBLIC_KEY: process.env.MP_PUBLIC_KEY
   });
 };
