a4cbaace1ad187c73ad1b4a4acbdabf686c51c4a
Denis Carvalho <47404461+deenisdee@users.noreply.github.com>
2026-03-21 21:46:02 -0300

    Update ui-components.js

--- js/ui-components.js
+++ js/ui-components.js
@@ -233,7 +233,8 @@ function renderTabbar(root) {
                                 <path d="M6 13.87A4 4 0 0 1 7.41 6a5.11 5.11 0 0 1 1.05-1.54 5 5 0 0 1 7.08 0A5.11 5.11 0 0 1 16.59 6 4 4 0 0 1 18 13.87V21H6Z"/>
                                 <line x1="6" y1="17" x2="18" y2="17"/>
                             </svg>
-                            <span>Vegan<span style="color: #16a34a;">fit</span><span style="opacity: 0.7;">.App</span></span>
+							<img src="image/logo-vegan.png" style="width: 50px; height:45px ;" />
+                            <span>Veganfit.<span style="color: #16a34a;">Life</span><span style="opacity: 0.7;"></span></span>
                         </div>
                         <button class="hamburger-close tap" onclick="window.closeHamburgerMenu && window.closeHamburgerMenu()" aria-label="Fechar menu">
                             <i data-lucide="x"></i>
